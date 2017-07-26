dab0057fa6913552a637942f64f0b2f896ca5ba0
arieled91 <arieled91@gmail.com>
2017-07-26 01:06:15 -0300

    fixed db loader

--- api/src/main/kotlin/com/openteam/application/persistance/DatabaseLoader.kt
+++ api/src/main/kotlin/com/openteam/application/persistance/DatabaseLoader.kt
@@ -23,13 +23,13 @@ class DatabaseLoader : CommandLineRunner {
 //        val isDev = this.environment.getActiveProfiles().contains("dev")
 //
 //        if(isDev) {
-            val event = this.eventDao.save(Event("Futbol", LocalDateTime.of(2017, 7, 7, 19, 0)))
-
-            event.teams.get(0).players.add(Player("Ariel", "ariel@gmail.com"))
-            event.teams.get(0).players.add(Player("Juan", "juan@hotmail.com"))
-            event.teams.get(0).players.add(Player("Carlos", "", true))
-
-            eventDao.save(event)
+//            val event = this.eventDao.save(Event("Futbol", LocalDateTime.of(2017, 7, 7, 19, 0)))
+//
+//            event.teams.get(0).players.add(Player("Ariel", "ariel@gmail.com"))
+//            event.teams.get(0).players.add(Player("Juan", "juan@hotmail.com"))
+//            event.teams.get(0).players.add(Player("Carlos", "", true))
+//
+//            eventDao.save(event)
 //        }
     }
 }
